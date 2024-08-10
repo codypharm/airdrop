658133aa93ca429018344edbd532eac43e94a3ea
Chukwunonso Ikeji <williamikeji@gmail.com>
2024-08-10 12:15:18 +0100

    done

--- script/Interact.s.sol
+++ script/Interact.s.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.24;
 
-import {Script} from "forge-std/Script.sol";
-import {DevOpsTools} from "foundry-devops/src/DevOpstools.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
 
 contract ClaimAirdrop is Script {
@@ -11,17 +11,35 @@ contract ClaimAirdrop is Script {
     uint256 CLAIMING_AMOUNT = 25 * 1e18;
     bytes32 PROOF_ONE = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
     bytes32 PROOFTWO = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
-    bytes32[] proof = [PROOF_ONE, PROOFTWO];
+    bytes32[] Proof = [PROOF_ONE, PROOFTWO];
+    //signature from cast without the 0X
+    bytes private SIGNATURE =
+        hex"fbd2270e6f23fb5fe9248480c0f4be8a4e9bd77c3ad0b1333cc60b5debc511602a2a06c24085d8d7c038bad84edc53664c8ce0346caeaa3570afec0e61144dc11c";
+
+    error ClaimAirdropScript__INVALID_SIGNATURE_LENGTH();
+
+    function splitSignature(bytes memory sig) public pure returns (uint8 v, bytes32 r, bytes32 s) {
+        if (sig.length != 65) {
+            revert ClaimAirdropScript__INVALID_SIGNATURE_LENGTH();
+        }
+
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+    }
 
     function claimAirDrop(address airdrop) public {
         vm.startBroadcast();
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(SIGNATURE);
         MerkleAirdrop(airdrop).claim(CLAIMING_ADDRESS, CLAIMING_AMOUNT, Proof, v, r, s);
-
         vm.stopBroadcast();
     }
 
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("MerkleAirDrop", block, chainid);
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("MerkleAirdrop", block.chainid);
+        // console.log(mostRecentlyDeployed);
         claimAirDrop(mostRecentlyDeployed);
     }
 }
